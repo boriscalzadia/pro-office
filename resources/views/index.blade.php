37762d00fd5adc3fb4e164941b5c743a815fda2c
sindobarrera <mezasindy@gmail.com>
2017-01-09 12:09:13 -0600

    interfaz

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,16 @@
 @extends('templates.main')
 @section('content')
 <div class="container">
-</div>
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading"> <h1>Bienvenidos a Pro-Office </h1></div>
+                <div class="panel-body">
+				 <img src="plugins/bootstrap/img/logo.png" class="img-rounded" alt="Pro-Office">
+                </div>
+                </div>
+                </div>
+                </div>
+                </div>
+
 @endsection
--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -11,6 +11,7 @@
 <body class="grey darken-4">
 	@include('templates.navbar')
     <div class="container">
+    <div class="col-md-8 col-md-8-offset-2">
     		<div class="panel-body">
     			@if (session()->has('flash_notification.message'))
 			    <div class="alert alert-{{ session('flash_notification.level') }}">
@@ -22,6 +23,8 @@
 			@include('templates.errors')
     		</div>
             @yield('content')
+    </div>
+    </div>
     </div>
 	<script src="{{ asset('js/jquery.js')}}"></script>
 	<script src="{{ asset('plugins/bootstrap/js/bootstrap.js')}}"></script>
