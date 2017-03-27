71af118a14c483ec157da1d0fd7a6faadf7fe2b3
boriscalzadia <boriscalzadia@gmail.com>
2017-03-27 17:52:27 -0600

    Barra de navegacion

--- resources/views/templates/navbar.blade.php
+++ resources/views/templates/navbar.blade.php
@@ -27,7 +27,7 @@
                             </li>
                             <li><a href="{{ route('servicios.index') }}">Servicios</a></li>
                             
-                            <li><a href="{{ route('provedores.index')}}">Consultas</a></li>
+                            <li><a href="{{ route('provedores.index')}}">Consultas a Clientes</a></li>
                             
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
